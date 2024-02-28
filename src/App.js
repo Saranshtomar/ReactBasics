1e891619a8cefa215f851928efef557f8714e31f
Saransh <saranshtomar926@gmail.com>
2024-02-28 22:42:34 +0530

    Basic Form Validation

--- src/App.js
+++ src/App.js
@@ -4,12 +4,51 @@ import User from "./User";
 import { useState } from "react";
 
 function App() {
-var value = 8;
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [ider,setIdEr] = useState(false)
+  const [nameer,setNameEr] = useState(false)
+
+  function submitHandler(e) {
+    e.preventDefault();
+  }
+
+  function idHandler(e){
+    const item= e.target.value
+    if(item.length<3)
+    {
+      setIdEr(true)
+    }
+    else
+    {
+      setIdEr(false)
+    }
+  }
+  function nameHandler(e){
+    const item= e.target.value
+    if(item.length<3)
+    {
+      setNameEr(true)
+    }
+    else
+    {
+      setNameEr(false)
+    }
+  }
 
   return (
     <div>
-        {value==1?<h1>saransh</h1>:value==2?<h2>utkarsh</h2>:<h1>omkar</h1>}
-    
+      <h1>Login</h1>
+      <br></br>
+      <form onSubmit={submitHandler}>
+        <label For="id">Id</label>
+        <input type="text" placeholder="Enter id" id="id" onChange={idHandler} />{ider && <span >enter valid id </span>}
+        <br />
+        <label For="name">Name</label>
+        <input type="text" placeholder="Enter name" id="name" onChange={nameHandler} />{nameer && <span>enter right name </span>}
+        <br />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
